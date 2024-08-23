188a272c98e5cb50e450edfcd4085c9900e5d2ef
waqaskhanroghani <mwaqaskhanroghani@gmail.com>
2024-08-23 10:55:18 +0500

    redesgin profile screen

--- src/screens/profile/index.js
+++ src/screens/profile/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {View, Text, Image, TouchableOpacity} from 'react-native';
 import styles from './styles'; // Import the styles
+import Icon from 'react-native-vector-icons/Ionicons'; // Import icons
+import Routes from '../../navigation/Routes';
 
-const Profile = () => {
+const Profile = ({navigation}) => {
   return (
     <View style={styles.container}>
       {/* Profile Picture */}
@@ -23,11 +25,35 @@ const Profile = () => {
         </Text>
       </View>
 
-      {/* Edit Button */}
+      {/* Edit Profile Button */}
       <TouchableOpacity
         style={styles.editButton}
         onPress={() => console.log('Edit Profile')}>
         <Text style={styles.editButtonText}>Edit Profile</Text>
+      </TouchableOpacity>
+
+      {/* Settings Button */}
+      <TouchableOpacity
+        style={styles.optionButton}
+        onPress={() => console.log('Settings')}>
+        <Icon
+          name="settings-outline"
+          size={20}
+          color={styles.optionButtonText.color}
+        />
+        <Text style={styles.optionButtonText}>Settings</Text>
+      </TouchableOpacity>
+
+      {/* Logout Button */}
+      <TouchableOpacity
+        style={styles.logoutButton}
+        onPress={() => navigation.navigate(Routes.LOGIN)}>
+        <Icon
+          name="log-out-outline"
+          size={20}
+          color={styles.logoutButtonText.color}
+        />
+        <Text style={styles.logoutButtonText}>Logout</Text>
       </TouchableOpacity>
     </View>
   );
